fc4d0cf9ffba4f379355dc9d2fa37af19209f2db
jgrandon <jgrandon@github.com>
2026-03-20 16:35:14 -0300

    fix (index): load home from component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import Home from "@/pages/home"
 
-import Danger from "@/pages/home/danger"
-import Purpouse from "@/pages/home/purpouse"
-import Disclaimer from "@/pages/home/disclaimer"
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export default function Home() {
-  return (
-    <div
-      className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
-    >
-      <Danger />
-      <Purpouse />
-      <Disclaimer />
-      {/*}
-        <Image
-        className={styles.logo}
-        src="/next.svg"
-        alt="Next.js logo"
-        width={100}
-        height={20}
-        priority
-      />
-      */}
-    </div>
-  );
+export default function Index() {
+  return (<Home />);
 }
